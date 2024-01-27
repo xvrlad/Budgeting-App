78f05965ee921643b32a16a643dfa25336cd92d1
xvrlad <xavladores@gmail.com>
2024-01-27 15:29:54 +1300

    Add port to env file

--- main.go
+++ main.go
@@ -1,10 +1,19 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
 
 func main() {
 	r := gin.Default()
